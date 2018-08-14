35fe18c09811c23b87b3754c6bb47a3c32534fd5
Md Rana Mahmud <ranamd0@gmail.com>
2018-08-14 23:10:13 +0600

    Templates added

--- sustsurveys/sustsurveys/urls.py
+++ sustsurveys/sustsurveys/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'sustsurveys.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+# Home page 
+# Landing page area
+    url(r'^$', TemplateView.as_view(template_name='visitor/landing-index.html'), name='landing_index'),
+    
+
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
